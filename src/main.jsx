7db66e75dd276043ad86aa14227602e90039aa35
unknown <deniz.uslu1925@gmail.com>
2026-03-25 21:12:32 +0100

    Standardized to lowercase app.jsx to fix case sensitivity issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app.jsx'
+import App from './App'
 
 // HTML'deki 'root' id'li div içerisine React uygulamamızı yerleştiriyoruz
 ReactDOM.createRoot(document.getElementById('root')).render(
